26e9d4f798b710cc29f18e93f70799ddb2a97e54
aa1583 <anupadhikari@my.unt.edu>
2023-06-23 21:25:20 -0500

    updated changes with branch

--- src/com/pack/domain/Child.java
+++ src/com/pack/domain/Child.java
@@ -1,4 +1,14 @@
 package com.pack.domain;
 
 public class Child {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
