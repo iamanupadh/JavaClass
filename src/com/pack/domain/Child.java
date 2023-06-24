351ba65ae6fa55193d2764466f0aef628beb5b79
aa1583 <anupadhikari@my.unt.edu>
2023-06-23 21:36:10 -0500

    updated changes to the master

--- src/com/pack/domain/Child.java
+++ src/com/pack/domain/Child.java
@@ -4,11 +4,21 @@ public class Child {
 
     private String name;
 
+    private int myVal;
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getMyVal() {
+        return myVal;
+    }
+
+    public void setMyVal(int myVal) {
+        this.myVal = myVal;
     }
 }
